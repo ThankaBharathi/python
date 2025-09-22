c80409360f69186944eccab3cb7ad5c427aa5bb0
Thanka Bharathi <thankabharathi0@gmail.com>
2025-09-22 11:24:52 +0530

    pattern7 completed

---
+++ patterns/pattern7.py
@@ -0,0 +1,15 @@
+n = int(input())
+for row in range(1,n+1):
+    for space in range(1, (n-row)+1):
+        print(" ",end="")
+    for cols in range(1,(row*2-1)+1):
+        print("*",end="")
+    print()
+
+'''
+      *
+     ***
+    *****
+   *******
+  *********  
+'''
