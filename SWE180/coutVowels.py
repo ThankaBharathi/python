8a64e3ea32a84ec0c6036a648f615e521b27bb50
Thanka Bharathi <thankabharathi0@gmail.com>
2025-10-02 12:20:42 +0530

    count_vowels

---
+++ SWE180/coutVowels.py
@@ -0,0 +1,15 @@
+
+'''
+s = "bbbbbbb"
+lis = ['a','e','i','o','u','A','E','I','O','U']
+count = 0
+for char in s:
+    if char in lis:
+        count += 1
+print(count)
+'''
+
+s = "Hello World"
+count = sum(1 for char in s if char.lower() in 'aeiou')
+print(count)
+
