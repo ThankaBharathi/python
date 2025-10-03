2d6f4b9d2524cec6c60e12468432af2254ca181e
Thanka Bharathi <thankabharathi0@gmail.com>
2025-10-03 12:41:00 +0530

    cols wise

--- SWE180/matrix/rowwise.py
+++ SWE180/matrix/rowwise.py
@@ -7,3 +7,14 @@ matrix = [
 for i in range(len(matrix)):
     for j in range(len(matrix[0])):
         print(matrix[i][j],end=" ")
+        
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        print(matrix[i][j],end=" ")
