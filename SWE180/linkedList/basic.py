1d88829dacde6cf0ec5c2762e8e5c9887496451d
Thanka Bharathi <thankabharathi0@gmail.com>
2025-10-05 20:46:43 +0530

    basic class and object for linkedlist

---
+++ SWE180/linkedList/basic.py
@@ -0,0 +1,19 @@
+class Form:
+    Name = None
+    Age = None
+    Gender = None
+    Adress = None
+
+xerox1 = Form()
+xerox1.Name = "Thanka"
+xerox1.Age = 21
+xerox1.Gender = "Male"
+xerox1.Adress = "1/51b, Bungalow street vilathikulam"
+
+xerox2 = Form()
+xerox2.Name = "Bharathi"
+xerox2.Age = 20
+xerox2.Gender = "Male"
+xerox2.Adress = "1/51b, Bungalow street vilathikulam"
+
+print(xerox1.Name)
