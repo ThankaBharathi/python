fd8be024e4cd1a26b63e65078c02fadefb08832e
Thanka Bharathi <thankabharathi0@gmail.com>
2025-10-06 10:31:58 +0530

    creating a node and linkedlist class

---
+++ SWE180/linkedList/singlyLinkedList.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.pointer = None
+
+class LinkedList:
+    def __init(self):
+        self.head = None   # atarting node head of a list 
+        
+    # Method to insert at end
+    
+    
+    
