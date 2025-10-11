e3fc9cf6153a9f8eeef74377aab69513d4f28355
Thanka Bharathi <thankabharathi0@gmail.com>
2025-10-11 21:18:43 +0530

    single ll

--- SWE180/linkedList/insertatMidle.py
+++ SWE180/linkedList/insertatMidle.py
@@ -10,7 +10,6 @@ class Solution:
             return head
         
         slow, fast = head, head
-        
         while fast.next and fast.next.next:
             slow = slow.next 
             fast = fast.next.    
