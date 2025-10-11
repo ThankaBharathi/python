963792a76d3590463ec296efb9c76d994377bffc
Thanka Bharathi <thankabharathi0@gmail.com>
2025-10-11 19:40:01 +0530

    insert at middle

---
+++ SWE180/linkedList/insertatMidle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def insertInMiddle(self, head, x):
+        newNode = Node(x)
+        if head is None:
+            return newNode
+        if head.next is Node:
+            head.next = newNode
+            return head
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next 
+            fast = fast.next.next 
+        newNode.next = slow.next 
+        slow.next = newNode
+        return head 
