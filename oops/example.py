499818f60b7266ce45a31bb6067245fe6a0ea9b6
Thanka Bharathi <thankabharathi0@gmail.com>
2025-10-30 09:09:06 +0530

    oops

---
+++ oops/example.py
@@ -0,0 +1,14 @@
+class Dad:
+    def house(self):
+        print("Pink")
+
+class Son(Dad):
+    def godown(self):
+        print("Brown")
+
+class Daughter(Dad):
+    def factory(self):
+        print("White")
+
+da = Daughter
+da
