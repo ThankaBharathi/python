d24a2986f8a80c68d8f30483b8af3850035bb3bd
Thanka Bharathi <thankabharathi0@gmail.com>
2025-10-31 19:43:31 +0530

    rorate the array by clockwise

---
+++ Arrays/clockwise_rotate_array.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+
+class Solution:
+    def rotate(self, arr):
+        temp = arr[-1]
+        for i in range(len(arr) -1,0,-1):
+            arr[i] = arr[i-1]
+        arr[0] = temp
+        return arr
+            
+    
