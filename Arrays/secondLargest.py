433336e67fa1db0b8cd07ecb81e01eb01dae6572
Thanka Bharathi <thankabharathi0@gmail.com>
2025-10-31 11:12:10 +0530

    second largest

--- Arrays/secondLargest.py
+++ Arrays/secondLargest.py
@@ -1,4 +1,13 @@
-
-name = "124536384"
-int(name)
-print(name.sort())
+class Solution:
+    def getSecondLargest(self, arr):
+        if len(arr) < 2:
+            return -1
+        first = second = float('-inf')
+        for num in arr:
+            if num > first:
+                second = first
+                first = num
+            elif num > second and num != first:
+                second = num 
+        return second if second != float('-inf') else -1
+        
