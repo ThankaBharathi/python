18db5cb250d46aec819b4b5f35849f4c27645836
Thanka Bharathi <thankabharathi0@gmail.com>
2025-11-01 21:35:09 +0530

    optimal solution

--- Arrays/sort_0_1_2_s.py
+++ Arrays/sort_0_1_2_s.py
@@ -11,7 +11,28 @@ class Solution:
         arr[:] = [0]*zero + [1]*one + [2]*two
         return arr
                 
+   # optimal solution             
+class Solution:
+    def sort012(self, arr):
+        low,mid,high = 0,0,len(arr)-1
+        while mid <= high:
+            if arr[mid] == 0:
+                arr[low],arr[mid] = arr[mid],arr[low]
+                low += 1
+                mid += 1
+            elif arr[mid] == 1:
+                mid += 1
+            else:
+                arr[high],arr[mid] = arr[mid],arr[high]
+                high -= 1
+        return arr
                 
+        
+                
+                
+        
+        
+        
         
         
         
