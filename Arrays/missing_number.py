e410949f0b65582f33497930e059e552f347f354
Thanka Bharathi <thankabharathi0@gmail.com>
2025-11-03 10:27:29 +0530

    optimal solution

--- Arrays/missing_number.py
+++ Arrays/missing_number.py
@@ -11,4 +11,17 @@ class Solution:
                 return i
                 
         
+class Solution:
+    def missingNum(self, arr):
+        n = len(arr)+1
+        xor_num = 0
+        xor_arr = 0
+        for i in range(1,n+1):
+            xor_arr ^= i
+        for num in arr:
+            xor_num ^= num
+        return xor_arr ^ xor_num
+        
+                
+        
    
