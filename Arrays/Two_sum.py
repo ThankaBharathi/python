8142ce443b72f6985dbf6c8ccf26e367421decc1
Thanka Bharathi <thankabharathi0@gmail.com>
2025-11-04 11:20:28 +0530

    for return the index

--- Arrays/Two_sum.py
+++ Arrays/Two_sum.py
@@ -12,3 +12,19 @@ for i in range(len(arr)):
         break
 if not found:
     print(False)
+
+
+arr = [0, -1, 2, -3, 8]
+target = 5
+found = False
+
+for i in range(len(arr)):
+    for j in range(i + 1, len(arr)):
+        if arr[i] + arr[j] == target:
+            print([i,j])
+            found = True
+            break
+    if found:
+        break
+if not found:
+    print(False)
