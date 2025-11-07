db02a8b64c44ea6d8ed5050f352131a9a65413ce
Thanka Bharathi <thankabharathi0@gmail.com>
2025-11-07 10:55:58 +0530

    first and last occurence

---
+++ binary_search/first_and_last_occurence.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+class Solution:
+    def find(self, arr, x):
+        left , right, first = 0,len(arr)-1,-1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == x:
+                first = mid
+                right = mid - 1
+            elif arr[mid] < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        left , right, second = 0,len(arr)-1,-1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == x:
+                second = mid
+                left = mid + 1
+            elif arr[mid] < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return [first, second]
+        
+        
+        
+            
+        
