c3ec929dfa03625f5ef0beb4a8578f7e0b534751
Thanka Bharathi <thankabharathi0@gmail.com>
2025-11-07 10:43:46 +0530

    floor of a number

---
+++ binary_search/floor_number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findFloor(self, arr, x):
+        left, right = 0, len(arr)-1
+        floor_index = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] <= x:
+                floor_index = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return floor_index
+                
+            
+        
+        
