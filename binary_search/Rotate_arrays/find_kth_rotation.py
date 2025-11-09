981104bdc4ba98b4e6f02922f7df48ba28c04933
Thanka Bharathi <thankabharathi0@gmail.com>
2025-11-09 10:41:47 +0530

    kth rotatations

--- binary_search/Rotate_arrays/find_kth_rotation.py
+++ binary_search/Rotate_arrays/find_kth_rotation.py
@@ -8,4 +8,25 @@ class Solution:
       
         
         
+class Solution:
+    def findKRotation(self, arr):
+        left, right = 0, len(arr) - 1
         
+        if arr[left] < arr[right]:
+            return 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if mid > 0 and arr[mid] < arr[mid - 1]:
+                return mid
+    
+            if mid < right and arr[mid] > arr[mid + 1]:
+                return mid + 1
+            
+            if arr[left] <= arr[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return 0  
